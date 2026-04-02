e94d0115fb96e71af0cb8e2154931244494405a7
AlifRizki30 <alifrizki.p30@gmail.com>
2026-04-02 16:20:59 +0700

    tes

---
+++ MODUL 4/soal.2.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+)
+
+func hitungSkor(waktu []int, soal *int, skor *int) {
+	*soal = 0
+	*skor = 0
+
+	for _, w := range waktu {
+		if w <= 301 {
+			*soal++
+			*skor += w
+		}
+	}
+}
+
+func main() {
+	var nama string
+	var pemenang string
+	maxSoal := -1
+	minWaktu := 1<<31 - 1
+
+	for {
+		fmt.Scan(&nama)
+
+		if nama == "Selesai" {
+			break
+		}
+		waktu := make([]int, 8)
+		for i := 0; i < 8; i++ {
+			fmt.Scan(&waktu[i])
+		}
+
+		var soal, skor int
+		hitungSkor(waktu, &soal, &skor)
+		if soal > maxSoal || (soal == maxSoal && skor < minWaktu) {
+			maxSoal = soal
+			minWaktu = skor
+			pemenang = nama
+		}
+	}
+	fmt.Println(pemenang, maxSoal, minWaktu)
+}
